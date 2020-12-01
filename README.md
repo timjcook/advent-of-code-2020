da43c9199ff5619a04d8cdb444e996d8a6ceae85
Tim Cook <timjcook@gmail.com>
2020-12-02 00:16:27 +1100

    Day 1, Challenge 2 - More depth to these anomalies

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Advent of Code 2020
+
+### Day 1 - ✅
+Focus was mainly on defining an object to describe the operations ie) the Parser and then once I saw Challenge 2, creating a solution that would scale to comparing any number of values.
+Performance was not a consideration and if the number of values was increased much more it would take ages 😆
--- main.rb
+++ main.rb
@@ -1,12 +1,13 @@
 class ExpenseReportParser
-  def initialize(report_path)
+  def initialize(report_path, num_values_to_compare)
     @report_path = report_path
+    @num_values_to_compare = num_values_to_compare
   end
 
-  attr_reader :report_path
+  attr_reader :report_path, :values_to_compare
 
-  def find_anomoly
-    compare_report_lines
+  def find_anomaly
+    compare_values(expense_report, @num_values_to_compare, [])
   end
 
   private
@@ -15,25 +16,42 @@ class ExpenseReportParser
     @expense_report ||= File.read(report_path).split.map(&:to_i)
   end
 
-  def is_anomoly?(value1, value2)
-    value1 + value2 == 2020
+  def is_anomaly?(values)
+    values.reduce(:+) == 2020
   end
 
-  def compare_report_lines
-    expense_report.each_with_index do |v1, index1|
-      expense_report.each_with_index do |v2, index2|
-        unless index1 == index2
-          if is_anomoly?(v1, v2)
-            return { value1: v1, value2: v2 }
-          end
-        end
+  def compare_values(report_lines, num_values_to_compare, current_values)
+    if (num_values_to_compare == 0)
+      if (is_anomaly?(current_values))
+        return current_values
+      else
+        return []
       end
     end
+
+    report_lines.each_with_index do |value, index|
+      remaining_report_lines = report_lines.reject.with_index { |n, i| i == index }
+
+      results = compare_values(
+        remaining_report_lines,
+        num_values_to_compare - 1,
+        [].push(current_values).push(value).flatten
+      )
+
+      return results if results.count > 0
+    end
+
+    return []
   end
 end
 
 
-parser = ExpenseReportParser.new('input-data/expense-report.txt')
-results = parser.find_anomoly
+parser = ExpenseReportParser.new('input-data/expense-report.txt', 3)
+results = parser.find_anomaly
 
-print (results[:value1] * results[:value2]).to_s + "\n"
+if (results.count == 0)
+  print "No results found.\n"
+else
+  print results.to_s + "\n"
+  print (results.reduce(:*)).to_s + "\n"
+end
