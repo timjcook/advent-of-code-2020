e8fe5a356c381b5c2bb98dcd78471ff640e0b919
Tim Cook <timjcook@gmail.com>
2020-12-03 00:39:29 +1100

    Day 2, Challenge 2 - A new type of Corruption Detector, classes coming in handy 🛷

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ### Day 1 - ✅
 Focus was mainly on defining an object to describe the operations ie) the Parser and then once I saw Challenge 2, creating a solution that would scale to comparing any number of values.
 Performance was not a consideration and if the number of values was increased much more it would take ages 😆
+
+### Day 2 - ✅
+Defining some objects paid off! Messed around with tagging the password and passing the valid check to a Detector object. Moving on to Challenge 2 was very simple, little bit of refactoring and defining a new type of Detector class with the same interface. Also a sneaky chance to bust out an XOR operator, don't get many situations where I can do that. Unfortunately tired brain originally added one to the range rather than subtracting to compensate for no zero indexing 🤷‍♀️
--- password-list-analyzer.rb
+++ password-list-analyzer.rb
@@ -1,48 +1,78 @@
 class PasswordListAnalyzer
+  attr_reader :policy
+
+  def initialize(policy)
+    @policy = policy
+  end
+
   def find
     tagged_passwords = password_list.map { |list_item|  TaggedPassword.new(list_item) }
 
+    corruption_detector = create_corruption_detector
     tagged_passwords.map do |tp|
-      cd = CorruptionDetector.new(tp)
       {
         password: tp.password,
-        valid: cd.analyze
+        valid: corruption_detector.analyze(tp)
       }
     end
   end
 
   private
 
+  def create_corruption_detector
+    if (policy == :sled)
+      create_sled_hire_corruption_detector
+    elsif (policy == :toboggan)
+      create_toboggan_hire_corruption_detector
+    else
+      raise "Invalid Policy!"
+    end
+  end
+
+  def create_sled_hire_corruption_detector
+    SledHireCorruptionDetector.new
+  end
+
+  def create_toboggan_hire_corruption_detector
+    TobogganHireCorruptionDetector.new
+  end
+
   def password_list
     @password_list ||= File.read('input-data/corrupt-passwords.txt').split("\n")
   end
 end
 
-class CorruptionDetector
-  attr_reader :tagged_password
-
-  def initialize(tagged_password)
-    @tagged_password = tagged_password
-  end
-
-  def analyze
+class SledHireCorruptionDetector
+  def analyze(tagged_password)
     required_letter = tagged_password.letter
     password = tagged_password.password
 
     required_letter_count = password.split('').count { |letter| letter == required_letter }
 
-    required_letter_count >= tagged_password.min_range && required_letter_count <= tagged_password.max_range
+    required_letter_count >= tagged_password.lower_number && required_letter_count <= tagged_password.upper_number
+  end
+end
+
+class TobogganHireCorruptionDetector
+  def analyze(tagged_password)
+    required_letter = tagged_password.letter
+    password = tagged_password.password
+
+    position_a = password[tagged_password.lower_number - 1] == required_letter
+    position_b = password[tagged_password.upper_number - 1] == required_letter
+
+    position_a ^ position_b
   end
 end
 
 class TaggedPassword
-  attr_reader :min_range, :max_range, :letter, :password
+  attr_reader :lower_number, :upper_number, :letter, :password
 
   def initialize(password_list_item)
     tagged_list_item = split_entry(password_list_item)
 
-    @min_range = tagged_list_item[:min_range].to_i
-    @max_range = tagged_list_item[:max_range].to_i
+    @lower_number = tagged_list_item[:lower_number].to_i
+    @upper_number = tagged_list_item[:upper_number].to_i
     @letter = tagged_list_item[:letter]
     @password = tagged_list_item[:password]
   end
@@ -55,15 +85,20 @@ class TaggedPassword
     split_range = split_list_item[0].split("-")
 
     {
-      min_range: split_range[0],
-      max_range: split_range[1],
+      lower_number: split_range[0],
+      upper_number: split_range[1],
       letter: split_list_item[1].delete_suffix(":"),
       password: split_list_item[2]
     }
   end
 end
 
-password_list_analyzer = PasswordListAnalyzer.new
-valid_passwords = password_list_analyzer.find.filter { |p| p[:valid] }
+sled_hire_analyzer = PasswordListAnalyzer.new(:sled)
+valid_sled_passwords = sled_hire_analyzer.find.filter { |p| p[:valid] }
 
-print valid_passwords.count.to_s + "\n"
+print "Valid sled hire passwords: " + valid_sled_passwords.count.to_s + "\n"
+
+toboggan_hire_analyzer = PasswordListAnalyzer.new(:toboggan)
+valid_toboggan_passwords = toboggan_hire_analyzer.find.filter { |p| p[:valid] }
+
+print "Valid sled hire passwords: " + valid_toboggan_passwords.count.to_s + "\n"
