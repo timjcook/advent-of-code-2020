fd16e874e619e7edf54e7dcca9b4127c75377495
Tim Cook <timjcook@gmail.com>
2020-12-03 23:09:13 +1100

    Day 3, Challenge 2 - Sub in the position updater

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Performance was not a consideration and if the number of values was increased mu
 
 ### Day 2 - ✅
 Defining some objects paid off! Messed around with tagging the password and passing the valid check to a Detector object. Moving on to Challenge 2 was very simple, little bit of refactoring and defining a new type of Detector class with the same interface. Also a sneaky chance to bust out an XOR operator, don't get many situations where I can do that. Unfortunately tired brain originally added one to the range rather than subtracting to compensate for no zero indexing 🤷‍♀️
+
+### Day 3 - ✅
+I think my attempt to break down into objects a bit smoother today, based on some Object Oriented related feedback from yesterday's challenge (which I've updated since, just for fun 🙌). Defining very specific objects and then explicitly passing them to other objects who essentially know nothing about them and their variations, only their interface. Example here is the PositionUpdater, pass any variation of that into a RoutePlanner and let it do the work. Once again, the setup from Challenge 1 meant that Challenge 2 was only some basic refactoring so OO is winning the day so far.
+Also, there was something quite satisfying about creating the Snow and Tree classes, not sure why ❄️🎄
+
+Sneaky TIL as well, I had to do `self.x = x + 3` even though I defined an `attr_accessor`, thought I could do `x += 3` or something smooth like that. Can't do it within the class though because ruby needs an explicit receiver of the `x=` method I created, it tries to create a local variable `x` but that's `nil` so it throws a `nil + integer` type error, (thanks https://stackoverflow.com/a/20124579/1725126). Could have just updated `@x` as well.
--- slope-route-planner.rb
+++ slope-route-planner.rb
@@ -44,15 +44,18 @@ class TerrainMapGenerator
 end
 
 class RoutePlanner
-  attr_reader :terrain_map
+  attr_reader :terrain_map, :position_updater
 
-  def initialize(terrain_map)
+  def initialize(terrain_map:, position_updater:)
     @terrain_map = terrain_map
+    @position_updater = position_updater
     @x = 0
     @y = 0
     @visited_tiles = []
 
-    terrain_map.each do |row|
+    terrain_map.each.with_index do |row, index|
+      next unless index == y
+
       mark_tile(row)
       move_tiles
     end
@@ -72,8 +75,10 @@ class RoutePlanner
   attr_accessor :x, :y
 
   def move_tiles
-    self.x = x + 3
-    self.y = y + 1
+    new_positions = position_updater.update(current_x: x, current_y: y)
+
+    self.x = new_positions[:x]
+    self.y = new_positions[:y]
   end
 
   def mark_tile(row)
@@ -84,10 +89,57 @@ class RoutePlanner
   end
 end
 
+class PositionUpdater
+  attr_reader :x_dist, :y_dist
+
+  def initialize(x_dist:, y_dist:)
+    @x_dist = x_dist
+    @y_dist = y_dist
+  end
+
+  def update(current_x:, current_y:)
+    {
+      x: current_x + x_dist,
+      y: current_y + y_dist
+    }
+  end
+end
+
 terrain_map_generator = TerrainMapGenerator.new(map_data_path: 'input-data/slope-terrain.txt')
 terrain_map = terrain_map_generator.terrain_map
 
-route_planner = RoutePlanner.new(terrain_map)
+route_planner_1 = RoutePlanner.new(
+  terrain_map: terrain_map,
+  position_updater: PositionUpdater.new(x_dist: 1, y_dist: 1)
+)
+route_planner_2 = RoutePlanner.new(
+  terrain_map: terrain_map,
+  position_updater: PositionUpdater.new(x_dist: 3, y_dist: 1)
+)
+route_planner_3 = RoutePlanner.new(
+  terrain_map: terrain_map,
+  position_updater: PositionUpdater.new(x_dist: 5, y_dist: 1)
+)
+route_planner_4 = RoutePlanner.new(
+  terrain_map: terrain_map,
+  position_updater: PositionUpdater.new(x_dist: 7, y_dist: 1)
+)
+route_planner_5 = RoutePlanner.new(
+  terrain_map: terrain_map,
+  position_updater: PositionUpdater.new(x_dist: 1, y_dist: 2)
+)
 
-print "Number of rows traversed: " + route_planner.num_rows_traversed.to_s + "\n"
-print "Number of trees hit: " + route_planner.num_trees_hit.to_s + "\n"
+print "Number of rows traversed: " + route_planner_1.num_rows_traversed.to_s + "\n"
+print "Number of trees hit with route 1: " + route_planner_1.num_trees_hit.to_s + "\n"
+print "Number of trees hit with route 2: " + route_planner_2.num_trees_hit.to_s + "\n"
+print "Number of trees hit with route 3: " + route_planner_3.num_trees_hit.to_s + "\n"
+print "Number of trees hit with route 4: " + route_planner_4.num_trees_hit.to_s + "\n"
+print "Number of trees hit with route 5: " + route_planner_5.num_trees_hit.to_s + "\n"
+print "\n"
+print "Magic sledding number: " + ([
+  route_planner_1.num_trees_hit,
+  route_planner_2.num_trees_hit,
+  route_planner_3.num_trees_hit,
+  route_planner_4.num_trees_hit,
+  route_planner_5.num_trees_hit
+].reduce(:*)).to_s + "\n"
